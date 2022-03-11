f0cd1fb5c2155c1585c39bce509612f7cca7a291
Leszek (Les) <leszek.modrzejewski@gmail.com>
2022-03-11 16:22:57 +0000

    navbar added within the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react'
+import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <nav>
+            <h1>Build a ZOO</h1>
+
+            <ul>
+                <li>
+                    <Link to = '#'>Find Animals</Link>
+                </li>
+                <li>
+                    <Link to = '#'>Visit my ZOO</Link>
+                </li>
+            </ul>
+        </nav>
+    </BrowserRouter>
+
   );
 }
 
