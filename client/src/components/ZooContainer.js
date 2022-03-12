429487b9c3ab2dd9a9aebf9de41fd7fd1beba484
Leszek (Les) <leszek.modrzejewski@gmail.com>
2022-03-12 15:50:03 +0000

    filtered animals day&night added

--- client/src/components/ZooContainer.js
+++ client/src/components/ZooContainer.js
@@ -1,15 +1,24 @@
 import ZooAnimal from "./ZooAnimal";
+ import { useState } from "react";
 
 const ZooContainer = ({zooAnimals, removeAnimal}) => {
 
-    const zooAnimalNodes = zooAnimals.map((animal) => {
-            return <ZooAnimal animal={animal} key={animal.id} removeAnimal={removeAnimal} />
-    })
+    const [showAnimals, setShowAnimals] = useState('')
 
+
+    const zooAnimalNodes = zooAnimals.filter(animal => {
+        if(animal.active_time.includes(showAnimals) ) {
+            return animal
+        }}).map((animal) => {
+        return <ZooAnimal animal={animal} key={animal.id} removeAnimal={removeAnimal} />
+        })
 
     return (
         <>
         <h2>Zoo Container</h2>
+        <button onClick={event => setShowAnimals(event.target.value)} value=''>Show All</button>
+        <button onClick={event => setShowAnimals(event.target.value)} value='Diurnal'>See Day Animals</button>
+        <button onClick={event => setShowAnimals(event.target.value)} value='Nocturnal'>Show Night Animals</button>
         <ul>
         {zooAnimalNodes}
         </ul>
