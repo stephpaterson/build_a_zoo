248628f8da370c0cb4a87c9fec4eeb3f5ffc7d46
Steph Paterson <steph.c.paterson@gmail.com>
2022-03-12 16:10:40 +0000

    rendered home page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import AnimalPageContainer from './containers/AnimalsPageContainer';
 import ZooPageContainer from './containers/ZooPageContainer';
+import HomePageContainer from './containers/HomePageContainer';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
 
         <Navbar />
         <Routes>
+            <Route path='/' element={<HomePageContainer />} />
             <Route path='/animals' element={<AnimalPageContainer />} />
             <Route path='/zoo' element={<ZooPageContainer />} />
         </Routes>
---
+++ client/src/containers/HomePageContainer.js
@@ -0,0 +1,7 @@
+const HomePageContainer = () => {
+    return(
+        <h1>Welcome!</h1>
+    )
+}
+
+export default HomePageContainer;
