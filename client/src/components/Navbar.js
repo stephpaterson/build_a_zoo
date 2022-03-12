ac3ab0651b90ca3dea7d440d3af7d5ed4e3072c8
Steph Paterson <steph.c.paterson@gmail.com>
2022-03-12 17:10:20 +0000

    added zoo keeper in nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import UserContext from '../context/UserContext';
 
 const Navbar = () => {
+
+    const {zooKeeperName} = useContext(UserContext)
 
     return (  
 
         <nav>
         <h1>Build a ZOO</h1>
+
+        <h2>{zooKeeperName}</h2>
 
         <ul>
             <li>
