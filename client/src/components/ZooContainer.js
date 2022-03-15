db6925a4ba2b520fececfe424accf3db4b17e89f
Leszek (Les) <leszek.modrzejewski@gmail.com>
2022-03-15 12:56:05 +0000

    background is changing depends on diurnal/nocturnal

--- client/src/components/ZooContainer.js
+++ client/src/components/ZooContainer.js
@@ -1,12 +1,15 @@
+import "./ZooContainer.css"
 import ZooAnimal from "./ZooAnimal";
 import { useState } from "react";
+import useToggle from '../hooks';
+
 import {Container , Row, Button } from "react-bootstrap"
 
 
 const ZooContainer = ({zooAnimals, removeAnimal}) => {
 
     const [showAnimals, setShowAnimals] = useState('')
-
+    const { isDarkMode, toggleDarkMode} = useToggle();
 
     const zooAnimalNodes = zooAnimals.filter(animal => {
         if(animal.active_time.includes(showAnimals) ) {
@@ -17,19 +20,19 @@ const ZooContainer = ({zooAnimals, removeAnimal}) => {
    
 
     return (
-        <>
+        <div className={isDarkMode ? "dark" : "light"}>
         <Container>
         <h2>Visit the animals in your zoo</h2>
-        <Button onClick={event => setShowAnimals(event.target.value)} value=''>Show all the animals</Button>
-        <Button onClick={event => setShowAnimals(event.target.value)} value='Diurnal'>See the animals active in the daytime</Button>
-        <Button onClick={event => setShowAnimals(event.target.value)} value='Nocturnal'>See the animals active at night</Button>
+        <Button onClick={event => setShowAnimals(event.target.value)}>Show all the animals</Button>
+        <Button onClick={event => {setShowAnimals(event.target.value); toggleDarkMode()}}  value='Diurnal'>See the animals active in the daytime</Button>
+        <Button onClick={event => {setShowAnimals(event.target.value); toggleDarkMode()}} value='Nocturnal'>See the animals active at night</Button>
         </Container>
         <Container>
             <Row md={3} sm={1}>
                 {zooAnimalNodes}
             </Row>
-        </Container>
-        </>
+        </Container>  
+        </div>
     )
 }
 
